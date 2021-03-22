816eb2345cd0805ffeca180ac6bdf3d55444127b
Tata-paweennuch <paweennuch.boonamnuay@gmail.com>
2021-03-22 11:16:37 +0200

    Add helper function to calculate distance

---
+++ src/helper/DistanceCalculation.ts
@@ -0,0 +1,19 @@
+// the Haversine formula
+// ref: https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula/27943#27943
+export const getDistanceFromLatLonInKm = (lat1: number,lon1: number,lat2: number,lon2: number) =>  {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+const deg2rad = (deg: number) => {
+  return deg * (Math.PI/180)
+}
